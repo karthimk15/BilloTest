6a2937b667f1e903b6334f73ee8de3c4cef52b42
Karthimksk <95969576+Karthimksk@users.noreply.github.com>
2025-12-10 00:17:05 +0530

    ENV Call

--- tests/smoke/shopSmoke.test.ts
+++ tests/smoke/shopSmoke.test.ts
@@ -7,6 +7,9 @@ import { viewItemHelp } from '../../Helper/ViewItemHelp';
 import { createBillHelp } from '../../Helper/CreateBillHelp';
 import { billViewHelp } from '../../Helper/ViewBillHelp';
 test ("Smoke Test Suite", async ({page})=>{
+    console.log("PHONE:", process.env.PHONE);
+console.log("PASSWORD:", process.env.PASSWORD);
+
     await loginHelp(page);
     await dashboardHelp(page);
     await sideBarHelp(page);
